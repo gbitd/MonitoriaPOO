2d5c43a129dfbf5063430fe6c7f1bb9a2803bc9f
gbitd <gbitdinger@proton.me>
2025-06-26 12:48:50 -0300

    Limpa aplication

--- calculadora-swing/src/main/java/com/gbitd/projeto/App.java
+++ calculadora-swing/src/main/java/com/gbitd/projeto/App.java
@@ -1,14 +1,5 @@
 package com.gbitd.projeto;
 
-import java.awt.Color;
-
-import javax.swing.JFrame;
-
-import javax.swing.WindowConstants;
-
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
 
