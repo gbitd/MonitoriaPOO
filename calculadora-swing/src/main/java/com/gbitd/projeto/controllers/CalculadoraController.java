d6b3c350e2935841a6cd9619fe0ab4e768a96a38
gbitd <gbitdinger@proton.me>
2025-07-03 23:58:07 -0300

    Implementa CalculadoraController

--- calculadora-swing/src/main/java/com/gbitd/projeto/controllers/CalculadoraController.java
+++ calculadora-swing/src/main/java/com/gbitd/projeto/controllers/CalculadoraController.java
@@ -1,11 +1,10 @@
 package com.gbitd.projeto.controllers;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import com.gbitd.projeto.model.CalculadoraModel;
 import com.gbitd.projeto.view.CalculadoraView;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.awt.List;
-import java.awt.event.ActionEvent;
 
 /**
  * CalculadoraController
@@ -17,36 +16,80 @@ public class CalculadoraController {
     private CalculadoraView view;
 
 
+    //Tratamento de evento para o botao igualdade deve ser tratado pelo controller, pois usa o model
     public CalculadoraController(CalculadoraModel model, CalculadoraView view){
         this.model = model;
         this.view = view;
-    }
-
-    public void configurarListeners(){
-
-        view.getBotao1().addActionListener(new ActionListener() {
+        view.getBotaoIgualdade().addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent actionEvent){
-                    linkBtn1();
+                    calcula();
+                    exibeResultado();
                 }
             });
-        view.getBotao2().addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent actionEvent){
-                    linkBtn2();
-                }
-            });
+
+
     }
 
-    public void identificaOperacao(){
+    public Character identificaOperacao(){
         String texto = view.getTextoVisor();
-        ArrayList<Character> listaDeCaracteres = new ArrayList<>();
-        for (int i = 0; i<texto.length(); i++) {
-            listaDeCaracteres.add(texto.charAt(i));
-        };
+        StringBuilder operando1 = new StringBuilder(); //Strings são imutáveis. Em casos de repetição como em concatenações, StringBuilders ou StringBuffers são mais ideais
+        StringBuilder operando2 = new StringBuilder();
+        Character operador = '\n';
 
-        while(listaDeCaracteres[i]!=)
 
+        int i = 0;
+
+        while(i<texto.length() && (Character.isDigit(texto.charAt(i)) || texto.charAt(i) == '.')){
+            operando1.append(texto.charAt(i));
+            i++;
+        }
+
+        // O primeiro caractere após o operando1 é o operador
+        if(i<texto.length()){
+            operador = texto.charAt(i);
+            i++;
+
+        }
+
+        while(i<texto.length() && (Character.isDigit(texto.charAt(i)) || texto.charAt(i) == '.')){
+            operando2.append(texto.charAt(i));
+            i++;
+        }
+
+        // Converte operando para String, e depois para double, e atribuí ao modelo
+        model.setOperador1(Double.parseDouble(operando1.toString()));
+        model.setOperador2(Double.parseDouble(operando2.toString()));
+
+        return operador;
+
+    }
+
+    public void calcula(){
+
+        Character operador = identificaOperacao();
+
+        switch (operador){
+            case '+':
+                model.soma();
+                break;
+            case '-':
+                model.subtracao();
+                break;
+            case '*':
+                model.multiplicacao();
+                break;
+            case '/':
+                model.divisao();
+                break;
+            default:
+                view.mensagemErro();
+
+        }
+    }
+
+    public void exibeResultado(){
+        view.setTextoVisor(String.valueOf(model.getResultado()));;
     }
 
 
