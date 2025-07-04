a5af50161ba5555e44532eba5d23b44c1a91d614
gbitd <gbitdinger@proton.me>
2025-07-04 00:04:09 -0300

    Remove exemplo redundante

--- calculadora-swing/src/main/java/com/gbitd/projeto/App.java
+++ calculadora-swing/src/main/java/com/gbitd/projeto/App.java
@@ -1,11 +1,15 @@
 package com.gbitd.projeto;
 
 import com.gbitd.projeto.view.*;
+import com.gbitd.projeto.controllers.*;
+import com.gbitd.projeto.model.*;
 
 
 public class App {
     public static void main(String[] args) {
-        new CalculadoraView();
+        CalculadoraModel model = new CalculadoraModel();
+        CalculadoraView view = new CalculadoraView();
+        CalculadoraController controller = new CalculadoraController(model, view);
 
     }
 }
